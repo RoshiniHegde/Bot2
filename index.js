935254cd0c73a099942159ad30c989401a649dfe
Roshini Hegde (roshegde) <roshegde@cisco.com>
2017-02-16 23:20:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Restify = require("restify");
+
+const server = Restify.createServer({
+    name: CafeBot
+});
+
+const port_num = 8080; // to be decided
+
+server.post('/practise',(req, res, next) => {
+    let {
+        status,
+        result
+    }=req.body;
+
+    if (status === 200) {
+        console.log("Recieved Status 200");
+    }
+
+    return next;
+});
+
+server.listen((process.env.PORT || port_num, function() {
+    console.log("Server up and running");
+}));
